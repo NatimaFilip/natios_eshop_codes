8bbecbf1018a603fcd0a2daf13531aee43cd6236
NatimaFilip <filip.h@natima.cz>
2026-02-03 03:16:19 -0800

    x

--- js/3_pages/index.js
+++ js/3_pages/index.js
@@ -14,20 +14,23 @@ function moveFooterBanners() {
 
 moveFooterBanners();
 
-function wrapBlogsInWrapper() {
-	if (!body.classList.contains("in-index")) {
-		return;
-	}
-	let newsItems = document.querySelectorAll(".news-item");
-	if (!newsItems || newsItems.length === 0) return;
-	let homepageBlogWrapper = document.createElement("div");
-	homepageBlogWrapper.className = "homepage-blog-wrapper-custom";
+if (body.classList.contains("is-test-eshop")) {
+	function wrapBlogsInWrapper() {
+		if (!body.classList.contains("in-index")) {
+			return;
+		}
+		let newsItems = document.querySelectorAll(".news-item");
+		if (!newsItems || newsItems.length === 0) return;
+		let homepageBlogWrapper = document.createElement("div");
+		homepageBlogWrapper.className = "homepage-blog-wrapper-custom";
 
-	// insert before 1st news item
-	let firstNewsItem = newsItems[0];
-	firstNewsItem.parentNode.insertBefore(homepageBlogWrapper, firstNewsItem);
+		// insert before 1st news item
+		let firstNewsItem = newsItems[0];
+		firstNewsItem.parentNode.insertBefore(homepageBlogWrapper, firstNewsItem);
 
-	for (let i = 0; i < newsItems.length; i++) {
-		homepageBlogWrapper.appendChild(newsItems[i]);
+		for (let i = 0; i < newsItems.length; i++) {
+			homepageBlogWrapper.appendChild(newsItems[i]);
+		}
 	}
+	wrapBlogsInWrapper();
 }
