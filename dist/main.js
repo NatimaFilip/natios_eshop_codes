25cdc5dc13aee478137ec6d0803fd6e74e4aff42
NatimaFilip <filip.h@natima.cz>
2026-02-03 02:55:17 -0800

    x

--- dist/main.js
+++ dist/main.js
@@ -3523,6 +3523,24 @@ function moveFooterBanners() {
 
 moveFooterBanners();
 
+function wrapBlogsInWrapper() {
+	if (!body.classList.contains("in-index")) {
+		return;
+	}
+	let newsItems = document.querySelectorAll(".news-item");
+	if (!newsItems || newsItems.length === 0) return;
+	let homepageBlogWrapper = document.createElement("div");
+	homepageBlogWrapper.className = "homepage-blog-wrapper-custom";
+
+	// insert before 1st news item
+	let firstNewsItem = newsItems[0];
+	firstNewsItem.parentNode.insertBefore(homepageBlogWrapper, firstNewsItem);
+
+	for (let i = 0; i < newsItems.length; i++) {
+		homepageBlogWrapper.appendChild(newsItems[i]);
+	}
+}
+
 
   // From: js/3_pages/ordering_process_0_repeating.js
 if (body.classList.contains("ordering-process")) {
