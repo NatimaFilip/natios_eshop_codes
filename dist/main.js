d90ed1c5343210ed4a22a8d10dff9b1228adcce1
NatimaFilip <filip.h@natima.cz>
2026-05-25 15:41:20 +0200

    x

--- dist/main.js
+++ dist/main.js
@@ -3183,6 +3183,29 @@ document.addEventListener("debouncedResize", function () {
 });
 
 
+  // From: js/2_components/page_title_j1.js
+function updatePageTitle() {
+	if (!csLang) {
+		return;
+	}
+	if (
+		document.body.classList.contains("type-category") ||
+		document.body.classList.contains("type-manufacturer-detail") ||
+		document.body.classList.contains("type-page")
+	) {
+		let h1 = $("h1").first();
+		let title = document.title;
+		// If the first h1 isn't equal to the page title,
+		if (h1.text() !== title) {
+			// Change h1 to page title but remove " - Natios" or " | Natios" if it's in there
+			title = title.replace(/ - Natios| - natios| \| Natios| \| natios/g, "");
+			h1.text(title);
+		}
+	}
+}
+updatePageTitle();
+
+
   // From: js/2_components/products_block_amount_to_cart.js
 function addAmountToCartInProductsBlock() {
 	let allProductsInProductsBlock = document.querySelectorAll(".products-block .product");
---
+++ js/2_components/page_title_j1.js
@@ -0,0 +1,20 @@
+function updatePageTitle() {
+	if (!csLang) {
+		return;
+	}
+	if (
+		document.body.classList.contains("type-category") ||
+		document.body.classList.contains("type-manufacturer-detail") ||
+		document.body.classList.contains("type-page")
+	) {
+		let h1 = $("h1").first();
+		let title = document.title;
+		// If the first h1 isn't equal to the page title,
+		if (h1.text() !== title) {
+			// Change h1 to page title but remove " - Natios" or " | Natios" if it's in there
+			title = title.replace(/ - Natios| - natios| \| Natios| \| natios/g, "");
+			h1.text(title);
+		}
+	}
+}
+updatePageTitle();
