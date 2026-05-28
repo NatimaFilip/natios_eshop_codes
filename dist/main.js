4a76d062bd09a0a8da35dce70f32a1e1681f2fdd
NatimaFilip <filip.h@natima.cz>
2026-05-28 11:40:26 +0200

    x

--- dist/main.js
+++ dist/main.js
@@ -2354,20 +2354,20 @@ function footerBottomEdit() {
 
 	footerBottom.prepend(footerSocials);
 
-	let signature = footer.querySelector("#signature");
-	if (!signature) return;
+	/* let signature = footer.querySelector("#signature");
+	if (!signature) return; */
 
-	const spanAnd = document.createElement("span");
+	/* const spanAnd = document.createElement("span");
 	spanAnd.textContent = " & ";
-	signature.appendChild(spanAnd);
+	signature.appendChild(spanAnd); */
 
-	const filipHosek = document.createElement("a");
+	/* const filipHosek = document.createElement("a");
 	filipHosek.href = "https://partneri.shoptet.cz/profesionalove/filip-hosek/";
 	filipHosek.target = "_blank";
 	filipHosek.rel = "noopener";
 	filipHosek.className = "footer-signature-filip-hosek";
 	filipHosek.textContent = "Filip Hošek";
-	signature.appendChild(filipHosek);
+	signature.appendChild(filipHosek); */
 }
 footerBottomEdit();
 
